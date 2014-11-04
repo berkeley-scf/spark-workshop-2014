e294be0f7fedd53c81f2967db09a76c184ad52cc
Christopher Paciorek <paciorek@scf.Berkeley.EDU>
2014-11-03 18:16:15 -0800

    edits to initial parts of sec 3

--- piCalc.py
+++ piCalc.py
@@ -3,10 +3,10 @@ import numpy.random as rand
 from operator import add
 from pyspark import SparkContext
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 3:
         print >> sys.stderr, "Usage: spark-submit piCalc.py <total_samples> <slices>"
         exit(-1)
-    sc = SparkContext() # sys.argv[1], "PythonPi")
+    sc = SparkContext() 
     total_samples = int(sys.argv[1]) if len(sys.argv) > 1 else 1000000
     num_slices = int(sys.argv[2]) if len(sys.argv) > 2 else 2
     samples_per_slice = round(total_samples / num_slices)
