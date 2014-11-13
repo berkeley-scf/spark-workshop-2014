1eb847d137eb3f91011ffda669e2518c10cea49d
Christopher Paciorek <paciorek@scf.Berkeley.EDU>
2014-11-12 17:34:34 -0800

    added details on sparkR

--- sparkR.R
+++ sparkR.R
@@ -8,7 +8,35 @@
 
 # remember SparkR needs to have been started specifying MASTER to point to the Spark URI
 
+library(help = SparkR) # this will show the SparkR functions available and then you can get the R help info for individual functions as usual in R
+
+
+##############################
+# reading airline data in
+##############################
+
+master <- system("cat /root/ephemeral-hdfs/conf/masters", intern = TRUE)
+lines <- textFile(sc, paste0("hdfs://", master, ":9000/data/airline"))
+
+count(lines)
+
+# no partition() in SparkR yet; here's a workaround
+
+createKey <- function(line) {
+  vals <- strsplit(line, ",")
+  return(paste(vals[1],vals[3], collapse='-'), line)
+}
+
+linesWithKey <- map(lines, createKey)
+linesPartitioned <- partitionBy(linesWithKey, numPartitions = 192)
+
+#myRdd <- map(myTextFileRdd, function(x) { list(x, x) })
+#partitioned <- partitionBy(myRdd, numPartitions = 10L)
+
+##############################
 # calculation of Pi example
+##############################
+
 
 num_slices <- 100
 rdd <- parallelize(sc, 1:num_slices, num_slices)
@@ -26,9 +54,3 @@ samples_per_sliceBr <- broadcast(sc, samples_per_slice)
 count <-  reduce(lapply(rdd, sample), '+')
 print(paste0("Pi is roughly ", (4.0 * count / (num_slices*samples_per_slice))))
 
-# reading airline data in
-
-master <- system("cat /root/ephemeral-hdfs/conf/masters", intern = TRUE)
-lines <- textFile(sc, paste0("hdfs://", master, ":9000/data/airline"))
-
-count(lines)
